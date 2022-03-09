efa255b1d0b764dad386068fd4f81fc4326fb1e7
Kai Fletcher <kaifletcher@gmail.com>
2022-03-09 14:57:18 +0000

    Verkefni 3-1

---
+++ bin/verkefni3_1.dart
@@ -0,0 +1,12 @@
+void main(List<String> arguments) {
+
+  num talan = 0;
+
+  for (num teljari = 1; teljari < 11; teljari++)
+    {
+      print(teljari);
+      talan = talan + teljari;
+
+    }
+  print (talan);
+}
